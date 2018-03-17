195789159f70862f98e83a46b76859c40fceb492
David McGrann <dpmcgrann@gmail.com>
2018-03-17 22:38:06 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchies][:known_for][:languages]
+		 programmer_hash[:dennis_ritchies][:languages]
 end
 
 def adding_matz
