2bb550815b0bdfb2ba915b78965c2e7303f85c92
David McGrann <dpmcgrann@gmail.com>
2018-03-17 22:59:17 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash.each {|k, v| v[:dennis_ritchie][:languages] == "C"}
+		 programmer_hash[:dennis_ritchie][:languages]
 
 end
 
